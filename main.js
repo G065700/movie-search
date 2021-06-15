7d7e7d713a10a5476699eee6b5b4086717752fdd
G065700 <g065700@naver.com>
2021-06-16 03:16:09 +0900

    update movie-search

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 const searchEl = document.querySelector('.main .search')
 const searchIconEl = document.querySelector('.main .searchIcon')
+const pEl = document.querySelector('p')
+
+// searchEl.addEventListener('focus', () => {
+//   if(searchEl.value.trim().length !== 0) {
+//     searchIconEl.classList.add('navy')
+//     pEl.classList.remove('visible')
+//   } else {
+//     searchIconEl.classList.remove('navy')
+//   }
+// })
+
+setInterval(() => {
+  if (searchEl.value.trim().length !== 0) {
+    pEl.classList.remove('visible')
+    // searchIconEl.classList.add('navy')
+  }
+}, 100)
 
 searchIconEl.addEventListener('click', () => {
-  if(searchEl.value.trim().length === 0) {
-    alert("검색어를 입력해 주세요")
+  if (searchEl.value.trim().length === 0) {
+    document.querySelector('p').classList.add('visible')
   } else {
     axios
       .get(`https://www.omdbapi.com/?apikey=f1b5abfc&s=${searchEl.value}`)
@@ -11,8 +28,34 @@ searchIconEl.addEventListener('click', () => {
         if (response.data.Response === "False") {
           location.href = `./movieSearchFail/fail.html?searchTerm=${searchEl.value}`
         } else {
-          location.href = `./movieSearchSucc/success.html?res=${response}`
+          localStorage.setItem('res', JSON.stringify(response))
+          location.href = `./movieSearchSucc/success.html?searchTerm=${searchEl.value}&resLen=${response.data.Search.length}&page=1`
         }
       })
   }
 })
+
+searchEl.addEventListener('keyup', e => {
+  if (e.keyCode === 13) {
+    // alert('123')
+    if (searchEl.value.trim().length === 0) {
+      document.querySelector('p').classList.add('visible')
+    } else {
+      axios
+        .get(`https://www.omdbapi.com/?apikey=f1b5abfc&s=${searchEl.value}`)
+        .then((response) => {
+          if (response.data.Response === "False") {
+            location.href = `./movieSearchFail/fail.html?searchTerm=${searchEl.value}`
+          } else {
+            localStorage.setItem('res', JSON.stringify(response))
+            location.href = `./movieSearchSucc/success.html?searchTerm=${searchEl.value}&resLen=${response.data.Search.length}&page=1`
+          }
+        })
+    }
+  }
+})
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   alert(searchEl.value)
+//   searchEl.value = ""
+// })
--- movieSearchSucc/success.js
+++ movieSearchSucc/success.js
@@ -1,3 +1,35 @@
-// const res = ${res}
+const res = JSON.parse(localStorage.getItem('res')).data.Search
+const resLen = res.length
 
-console.log($(res))
+const movieSearchRes = document.querySelector('.result')
+
+const searchTerm = decodeURI(window.location.href).split(/(?=\&)/)[0].split(/(?<=\=)/)[1]
+const searchResLen = decodeURI(window.location.href).split(/(?=\&)/)[1].split(/(?<=\=)/)[1]
+const pageIdx = decodeURI(window.location.href).split(/(?=\&)/)[2].split(/(?<=\=)/)[1]
+
+movieSearchRes.innerHTML = "<p><span>\"" + searchTerm + "\"</span> 검색 결과 총 <span>" + searchResLen + " </span>개</p>"
+
+const movieListEl = document.getElementById('movieList')
+
+
+for (let i = (pageIdx * 8) - 7; i <= (pageIdx * 8); i++) {
+  let movieEl = document.createElement('div')
+  movieEl.classList.add('movie')
+  movieEl.innerHTML =
+    "<img src=" + res[i-1].Poster + " class=\"moviePoster\" />" +
+    "<p class=\"movieTitle\">" + res[i-1].Title + "</p>" +
+    "<div class=\"movieYear\">" + res[i-1].Year + "</div>"
+  
+  movieListEl.appendChild(movieEl)
+
+  if (i === resLen) break
+}
+
+
+// Pagination
+const paginationEl = document.querySelector('.pagination')
+
+for (let i = 1; i <= parseInt(resLen / 8) + 1; i++) {
+  paginationEl.innerHTML += 
+    "<a href=\"../movieSearchSucc/success.html?searchTerm=" + searchTerm + "&resLen=" + searchResLen + "&page=" + i + "\">" + i + "</a>"
+}
